346aabb5289a860890ba5f21d110aad708c04e46
Michael Scharf <mwscharf@ucdavis.edu>
2022-08-23 01:31:18 -0700

    media queries

--- src/components/Folder/Folder.js
+++ src/components/Folder/Folder.js
@@ -12,6 +12,7 @@ import {
   addItem,
   openFolder,
 } from "../../features/links/linkSlice";
+import MediaQuery from "react-responsive";
 
 const Folder = ({ index, name, items, id, isOpen }) => {
   const links = useSelector(selectLinks);
@@ -90,98 +91,204 @@ const Folder = ({ index, name, items, id, isOpen }) => {
 
   return (
     <>
-      {isOpen ? (
-        <div className="entireFolderWithContent">
-          <div className="openFolder">
-            <div className="buttonNextToFolderDiv">
-              <a onClick={() => handleOpenAll()} className="smallBtn">
-                a
-              </a>
-              <a onClick={() => handleFolderDelete(id)} className="smallBtn">
-                x
-              </a>
-            </div>
-            <img
-              onClick={handleOpenFolder}
-              className="svg"
-              src={openFolderSvg}
-              alt="an outline of a folder"
-            ></img>
-            {/* <div className="folderItems"> */}
-            <p className="folderNameOpen">{name}</p>
+      <MediaQuery minWidth={700}>
+        {isOpen ? (
+          <div className="entireFolderWithContent">
+            <div className="openFolder">
+              <div className="buttonNextToFolderDiv">
+                <a onClick={() => handleOpenAll()} className="smallBtn">
+                  a
+                </a>
+                <a onClick={() => handleFolderDelete(id)} className="smallBtn">
+                  x
+                </a>
+              </div>
+              <img
+                onClick={handleOpenFolder}
+                className="svg"
+                src={openFolderSvg}
+                alt="an outline of a folder"
+              ></img>
+              {/* <div className="folderItems"> */}
+              <p className="folderNameOpen">{name}</p>
 
+              {showFormState ? (
+                <form
+                  className="folderForm"
+                  onSubmit={(e) => handleAddItem(e)}
+                  action=""
+                >
+                  <span
+                    onClick={(e) => handleShowInput(e)}
+                    className="smallBtn"
+                  >
+                    -
+                  </span>
+                  <input
+                    onChange={handleInputChange}
+                    type="text"
+                    name="title"
+                    placeholder="title"
+                    value={itemInputState.title}
+                    className="folder-input"
+                  />
+                  <input
+                    onChange={handleInputChange}
+                    type="text"
+                    name="link"
+                    placeholder="link"
+                    value={itemInputState.link}
+                    className="folder-input"
+                  />
+                  <button className="submitBtn">Submit</button>
+                </form>
+              ) : (
+                <span className="smallBtn" onClick={handleShowInput}>
+                  +
+                </span>
+              )}
+            </div>
+            <div className="itemsDiv">
+              {newItems.length >= 1
+                ? newItems.map((item, i) => (
+                    <Item
+                      itemIndex={i}
+                      key={item.id}
+                      folderIndex={index}
+                      //index is referring to the index of the folder
+                      title={item.title}
+                      link={item.link}
+                      id={item.id}
+                      itemState={itemState}
+                      setItemState={setItemState}
+                    />
+                  ))
+                : null}
+            </div>
+          </div>
+        ) : (
+          <div className="entireClosedFolder">
+            <div className="closedFolder">
+              <div className="buttonNextToFolderDiv">
+                <a onClick={() => handleOpenAll()} className="smallBtn">
+                  a
+                </a>
+                <a onClick={() => handleFolderDelete(id)} className="smallBtn">
+                  x
+                </a>
+              </div>
+              <img
+                alt="an outline of a folder"
+                onClick={handleOpenFolder}
+                className="svg"
+                src={closedFolder}
+              ></img>
+              <p className="folderName">{name}</p>
+            </div>
+          </div>
+        )}
+      </MediaQuery>
+      <MediaQuery maxWidth={699}>
+        {isOpen ? (
+          <div className="entireFolderWithContent">
+            <div className="openFolder">
+              <div className="buttonNextToFolderDiv">
+                <a onClick={() => handleOpenAll()} className="smallBtn">
+                  a
+                </a>
+                <a onClick={() => handleFolderDelete(id)} className="smallBtn">
+                  x
+                </a>
+              </div>
+              <img
+                onClick={handleOpenFolder}
+                className="svg"
+                src={openFolderSvg}
+                alt="an outline of a folder"
+              ></img>
+              {/* <div className="folderItems"> */}
+              <p className="folderNameOpen">{name}</p>
+            </div>
             {showFormState ? (
               <form
                 className="folderForm"
                 onSubmit={(e) => handleAddItem(e)}
                 action=""
               >
-                <span onClick={(e) => handleShowInput(e)} className="smallBtn">
-                  -
-                </span>
-                <input
-                  onChange={handleInputChange}
-                  type="text"
-                  name="title"
-                  placeholder="title"
-                  value={itemInputState.title}
-                  className="folder-input"
-                />
-                <input
-                  onChange={handleInputChange}
-                  type="text"
-                  name="link"
-                  placeholder="link"
-                  value={itemInputState.link}
-                  className="folder-input"
-                />
+                <div>
+                  <span
+                    onClick={(e) => handleShowInput(e)}
+                    className="smallBtn formMinusBtn"
+                  >
+                    -
+                  </span>
+                  <input
+                    onChange={handleInputChange}
+                    type="text"
+                    name="title"
+                    placeholder="title"
+                    value={itemInputState.title}
+                    className="folder-input titleInputMobile"
+                  />
+                </div>
+                <div>
+                  <input
+                    onChange={handleInputChange}
+                    type="text"
+                    name="link"
+                    placeholder="link"
+                    value={itemInputState.link}
+                    className="folder-input linkInputMobile"
+                  />
+                </div>
                 <button className="submitBtn">Submit</button>
               </form>
             ) : (
-              <span className="smallBtn" onClick={handleShowInput}>
+              <span className="smallBtn addItemBtn" onClick={handleShowInput}>
                 +
               </span>
             )}
-          </div>
-          <div className="itemsDiv">
-            {newItems.length >= 1
-              ? newItems.map((item, i) => (
-                  <Item
-                    itemIndex={i}
-                    key={item.id}
-                    folderIndex={index}
-                    //index is referring to the index of the folder
-                    title={item.title}
-                    link={item.link}
-                    id={item.id}
-                    itemState={itemState}
-                    setItemState={setItemState}
-                  />
-                ))
-              : null}
-          </div>
-        </div>
-      ) : (
-        <div className="entireClosedFolder">
-          <div className="closedFolder">
-            <div className="buttonNextToFolderDiv">
-              <a onClick={() => handleOpenAll()} className="smallBtn">
-                a
-              </a>
-              <a onClick={() => handleFolderDelete(id)} className="smallBtn">
-                x
-              </a>
+
+            <div className="itemsDiv">
+              {newItems.length >= 1
+                ? newItems.map((item, i) => (
+                    <Item
+                      itemIndex={i}
+                      key={item.id}
+                      folderIndex={index}
+                      //index is referring to the index of the folder
+                      title={item.title}
+                      link={item.link}
+                      id={item.id}
+                      itemState={itemState}
+                      setItemState={setItemState}
+                    />
+                  ))
+                : null}
             </div>
-            <img
-              alt="an outline of a folder"
-              onClick={handleOpenFolder}
-              className="svg"
-              src={closedFolder}
-            ></img>
-            <p className="folderName">{name}</p>
           </div>
-        </div>
-      )}
+        ) : (
+          <div className="entireClosedFolder">
+            <div className="closedFolder">
+              <div className="buttonNextToFolderDiv">
+                <a onClick={() => handleOpenAll()} className="smallBtn">
+                  a
+                </a>
+                <a onClick={() => handleFolderDelete(id)} className="smallBtn">
+                  x
+                </a>
+              </div>
+              <img
+                alt="an outline of a folder"
+                onClick={handleOpenFolder}
+                className="svg"
+                src={closedFolder}
+              ></img>
+              <p className="folderName">{name}</p>
+            </div>
+          </div>
+        )}
+      </MediaQuery>
     </>
   );
 };
