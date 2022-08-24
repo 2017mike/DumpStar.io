8e34a15b70e2a4caff62eb4a4598e977f712b787
Michael Scharf <mwscharf@ucdavis.edu>
2022-08-23 21:17:04 -0700

    server

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const { join } = require("path");
 const app = express();
 
 app.use(express.static(join(__dirname, "client", "build")));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.get("*", (req, res) =>
   res.sendFile(join(__dirname, "client", "build", "index.html"))
