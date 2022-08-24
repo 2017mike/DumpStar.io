039369b3bfa762e4089649d4a19f4b8375371ad8
Michael Scharf <mwscharf@ucdavis.edu>
2022-08-23 17:22:16 -0700

    build route again

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../build")));
+  app.use(express.static(path.join(__dirname, "..", "build/index.html")));
   app.get("/*", function (req, res) {
     res.sendFile(path.join(__dirname, "../build/index.html"));
   });
