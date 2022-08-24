c2c03c160e26287b0391fadcee948fdfc5ae1caf
Michael Scharf <mwscharf@ucdavis.edu>
2022-08-23 17:24:29 -0700

    server

--- server/server.js
+++ server/server.js
@@ -5,12 +5,9 @@ publicPath = path.join(__dirname, "..", "build");
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "..", "build/index.html")));
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../build/index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
 
 app.listen(port, () => {
   console.log("Server is up!");
