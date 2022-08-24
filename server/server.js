7a4cff3900ad7fa5eb33537d4b5b8d5f4bef77c0
Michael Scharf <mwscharf@ucdavis.edu>
2022-08-23 17:31:14 -0700

    server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
-const path = require("path");
 const express = require("express");
+const path = require("path");
+
 const app = express();
-publicPath = path.join(__dirname, "..", "build");
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../build")));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/build/index.html"));
+  res.sendFile(path.join(__dirname + "../build/index.html"));
 });
 
-app.listen(port, () => {
-  console.log("Server is up!");
-});
+const port = process.env.PORT || 3000;
+app.listen(port);
+
+console.log(`Password generator listening on ${port}`);
